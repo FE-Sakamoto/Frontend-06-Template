15fbcf48fdc16010ef4367e788365414ae57e0b4
BugMaker.Yu <BugMaker.Yu@gmail.com>
2020-12-28 19:52:10 +0800

    :new: 添加css 解析库

--- Week 09/server.js
+++ Week 09/server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const fs = require('fs')
 
 http.createServer((request, response)=>{
   let body = []
@@ -11,45 +12,8 @@ http.createServer((request, response)=>{
   })
   .on('end', ()=>{
     body = (Buffer.concat([ Buffer.from(body.toString()) ])).toString();
-    console.log('body:', body)
     response.writeHead('200', {'Content-Type': 'text/html'})
-    response.end(`<html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <style>
-        #root {
-          width: 100vw;
-          height: 100vh;
-          background-color: lightgray;
-        }
-        .outter {
-          width: 100px;
-          height: 100px;
-          background-color: lightblue;
-        }
-        .inner {
-          width: 50px;
-          height: 50px;
-          background-color: lightyellow;
-        }
-        .text {
-          font-size: 24px;
-        }
-      </style>
-      <title>Document</title>
-    </head>
-    <body>
-      <div id="root">
-        <div class="outter">
-          <div class="inner" />
-        </div>
-        <div class="outter"></div>
-        <div class="text">hello world</div>
-        I'm Text Node
-      </div>
-    </body>
-    </html>`)
+    response.end(fs.readFileSync('./index.html', 'utf-8'))
   })
 }).listen(8000)
 
