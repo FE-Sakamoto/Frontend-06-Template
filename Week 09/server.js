592b8bde18b0f81a6e9452a53ad17c0e718f9b43
BugMaker.Yu <BugMaker.Yu@gmail.com>
2020-12-28 15:14:55 +0800

    :new: 修改server返回的html模板

--- Week 09/server.js
+++ Week 09/server.js
@@ -13,7 +13,43 @@ http.createServer((request, response)=>{
     body = (Buffer.concat([ Buffer.from(body.toString()) ])).toString();
     console.log('body:', body)
     response.writeHead('200', {'Content-Type': 'text/html'})
-    response.end(' Hello world\n')
+    response.end(`<html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <style>
+        #root {
+          width: 100vw;
+          height: 100vh;
+          background-color: lightgray;
+        }
+        .outter {
+          width: 100px;
+          height: 100px;
+          background-color: lightblue;
+        }
+        .inner {
+          width: 50px;
+          height: 50px;
+          background-color: lightyellow;
+        }
+        .text {
+          font-size: 24px;
+        }
+      </style>
+      <title>Document</title>
+    </head>
+    <body>
+      <div id="root">
+        <div class="outter">
+          <div class="inner" />
+        </div>
+        <div class="outter"></div>
+        <div class="text">hello world</div>
+        I'm Text Node
+      </div>
+    </body>
+    </html>`)
   })
 }).listen(8000)
 
